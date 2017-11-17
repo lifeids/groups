c0e582b41057890d4cfa35975d4ddabf673f0f48
lifeids <alhad@lifeids.com>
2017-11-17 12:54:22 +0530

    Add files via upload

--- src/Groups.php
+++ src/Groups.php
@@ -1,1 +1,104 @@
+<?php
 
+namespace Lifeids\Groups;
+
+use Lifeids\Groups\Models\Comment;
+use Lifeids\Groups\Models\Group;
+use Lifeids\Groups\Models\Post;
+use Lifeids\Groups\Models\User;
+
+class Groups
+{
+    public function __construct(Comment $comment, Group $group, Post $post, User $user)
+    {
+        $this->comment = $comment;
+        $this->group = $group;
+        $this->post = $post;
+        $this->user = $user;
+    }
+
+    /**
+     * Returns User instance with group relation.
+     *
+     * @param int $userId
+     *
+     * @return User
+     */
+    public function getUser($userId)
+    {
+        return $this->user->find($userId);
+    }
+
+    /**
+     * Creates a group.
+     *
+     * @param int   $user_id owner of group
+     * @param array $data    group information
+     *
+     * @return Group
+     */
+    public function create($user_id, $data)
+    {
+        return $this->group->make($user_id, $data);
+    }
+
+    /**
+     * Returns a group.
+     *
+     * @param int $groupId
+     *
+     * @return Group
+     */
+    public function group($groupId)
+    {
+        return $this->group->findOrFail($groupId);
+    }
+
+    /**
+     * Creates a post.
+     *
+     * @param array $data
+     *
+     * @return Post
+     */
+    public function createPost($data)
+    {
+        return $this->post->make($data);
+    }
+
+    /**
+     * Returns a post.
+     *
+     * @param int $postId
+     *
+     * @return Post
+     */
+    public function post($postId)
+    {
+        return $this->post->findOrFail($postId);
+    }
+
+    /**
+     * Adds a comment.
+     *
+     * @param array $comment
+     *
+     * @return Comment
+     */
+    public function addComment($comment)
+    {
+        return $this->comment->add($comment);
+    }
+
+    /**
+     * Returns a comment.
+     *
+     * @param int $commentId
+     *
+     * @return Comment
+     */
+    public function comment($commentId)
+    {
+        return $this->comment->findOrFail($commentId);
+    }
+}
---
+++ src/GroupsServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lifeids\Groups;
+
+use Illuminate\Support\ServiceProvider;
+
+class GroupsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    public function boot()
+    {
+        $this->registerAssets();
+    }
+
+    public function register()
+    {
+        $this->registerGroups();
+    }
+
+    private function registerGroups()
+    {
+        $this->app->bind('groups', function () {
+            return $this->app->make('Lifeids\Groups\Groups');
+        });
+    }
+
+    public function registerAssets()
+    {
+        $this->publishes([
+            __DIR__.'/migrations' => database_path('/migrations'),
+        ]);
+    }
+}
